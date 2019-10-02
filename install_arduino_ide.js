ce0e5d0b1565aae3f6d0da317ad99875c211a0d6
me-no-dev <hristo@espressif.com>
2019-10-03 00:11:56 +0300

    asdads

--- install_arduino_ide.js
+++ install_arduino_ide.js
@@ -1,5 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
+const io = require('@actions/io');
+const tc = require('@actions/tool-cache');
 const wait = require('./wait');
 
 
@@ -8,19 +11,19 @@ async function run() {
   try { 
     const ide_path = core.getInput('ide_path');
     const usr_path = core.getInput('usr_path');
-    console.log(`IDE_PATH: ${ide_path}, USR_PATH: ${usr_path}`)
+    const os_type = process.platform;
+    console.log(`IDE_PATH: ${ide_path}, USR_PATH: ${usr_path}, OS_TYPE: ${os_type}`)
 
     core.debug((new Date()).toTimeString())
     wait(1000);
     core.debug((new Date()).toTimeString())
 
-    core.setOutput('ide_path', ide_path);
-    core.setOutput('usr_path', usr_path);
-    core.exportVariable('this_is_STUPID', (new Date()).toTimeString());
-    
+    core.exportVariable('ARDUINO_IDE_PATH', ide_path);
+    core.exportVariable('ARDUINO_USR_PATH', usr_path);
+
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
   } 
   catch (error) {
     core.setFailed(error.message);
