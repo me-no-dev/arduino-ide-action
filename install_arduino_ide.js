12b68c2a5a283bf5128f9a22379e9317ecc183cb
me-no-dev <hristo@espressif.com>
2019-10-03 12:39:51 +0300

    Update install_arduino_ide.js

--- install_arduino_ide.js
+++ install_arduino_ide.js
@@ -13,15 +13,16 @@ async function run() {
     const os_type = process.platform;
     const os_arch = process.arch;
     const home = (os_type === "win32")?(process.env['HOMEDRIVE'] + process.env['HOMEPATH']):process.env['HOME'];
+    const path_delimiter = (os_type === "win32")?'\\':'/';
 
     var arduino_ide = ide_path;
     if (!arduino_ide.startsWith("/")){
-      arduino_ide = home + "/" + arduino_ide;
+      arduino_ide = home + path_delimiter + arduino_ide;
     }
 
     var arduino_usr = usr_path;
     if (!arduino_usr.startsWith("/")){
-      arduino_usr = home + "/" + arduino_usr;
+      arduino_usr = home + path_delimiter + arduino_usr;
     }
 
     console.log(`IDE_PATH: ${arduino_ide}, USR_PATH: ${arduino_usr}, OS_TYPE: ${os_type}, OS_ARCH: ${os_arch}`)
@@ -39,6 +40,7 @@ async function run() {
       await io.mv(archive, 'arduino.tar.xz');
       await exec.exec('tar', ['xf', 'arduino.tar.xz']);
       await io.mv('arduino-nightly', arduino_ide);
+      await io.rmRF('arduino.tar.xz');
     } else {
       await io.mv(archive, 'arduino.zip');
       if(os_type === "darwin"){
@@ -49,8 +51,10 @@ async function run() {
         arduino_ide += "/Contents/Java"
       } else {
         const arduino_unzip = await tc.extractZip('arduino.zip', 'arduino_unzip'); // archive_path, dst_path
-        await io.cp(arduino_unzip + "/arduino-nightly", arduino_ide, { recursive: true, force: false });
+        await io.cp(arduino_unzip + path_delimiter + "arduino-nightly", arduino_ide, { recursive: true, force: false });
+        await io.rmRF(arduino_unzip);
       }
+      await io.rmRF('arduino.zip');
     }
 
     await io.mkdirP(arduino_usr+'/libraries');
